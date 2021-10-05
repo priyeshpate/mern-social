a3dd8370a9cb9494e7ef6ff1a131b3f717f8842d
priyeshpate <91793536+priyeshpate@users.noreply.github.com>
2021-10-05 14:56:44 +0530

    Update webpack.config.server.js

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -18,8 +18,7 @@ const config = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: [ 'babel-loader' ]
-            },
+                           },
             {
                 test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
                 use: 'file-loader'
